a99264dc44034b94fc422addc7e78a8b8a3adb92
Rockhush237 <91887560+Rockhush237@users.noreply.github.com>
2023-08-12 14:26:25 +0530

    Update Rockhush.py

--- Rockhush.py
+++ Rockhush.py
@@ -1,4 +1,4 @@
 from os import system # chala ja bhosdike madarchod
-system ("git clone https://github.com/Rockhush237/Rockhush && cd Rockhush && python3 -m Am")
+system ("git clone https://github.com/Rockhush237/Rockhush && cd Rockhush && python3 -m AnonX")
 # try af
 print ("YOU GOT ERROR SAVE THE LOGS AND GIVE THIS LOGS TO @AM_YTBOTT")
