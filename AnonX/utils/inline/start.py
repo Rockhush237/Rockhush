e13178f613b0f18410f14631266471191e2e58f3
Rockhush237 <91887560+Rockhush237@users.noreply.github.com>
2023-08-12 15:27:46 +0530

    Update start.py

--- AnonX/utils/inline/start.py
+++ AnonX/utils/inline/start.py
@@ -1,4 +1,7 @@
+from typing import Union
+
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+
 import config
 
 def start_pannel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
