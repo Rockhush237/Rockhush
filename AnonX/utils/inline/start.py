35d15d7d33598e75a080dabfb6c590c8a890ed9a
Rockhush237 <91887560+Rockhush237@users.noreply.github.com>
2023-08-12 13:42:06 +0530

    Update start.py

--- AnonX/utils/inline/start.py
+++ AnonX/utils/inline/start.py
@@ -30,26 +30,34 @@ def private_panel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
     buttons = [
         [
             InlineKeyboardButton(
-                text="ᴀᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴘ",
+                text="➕ ᴀᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴘ",
                 url=f"https://t.me/{BOT_USERNAME}?startgroup=true",
             )
         ],
         [
             InlineKeyboardButton(
-                text="ʜᴇʟᴩ", callback_data="settings_back_helper"
-            )
-        ],
-        [
-            InlineKeyboardButton(
-                text="❣ sᴜᴩᴩᴏʀᴛ ❣", url=config.SUPPORT_GROUP
+                text="ᴏᴜʀ ᴡᴏʀᴅ 💌", url=config.SUPPORT_GROUP
             ),
             InlineKeyboardButton(
-                text="🥀 ᴍᴀɪɴᴛᴀɪɴᴇʀ 🥀", user_id=OWNER
+                text="ᴏᴡɴᴇʀ 💕", user_id=OWNER
             )
         ],
         [
             InlineKeyboardButton(
-                text="✨ sᴏᴜʀᴄᴇ ✨", url=config.UPSTREAM_REPO
+                text="ʜᴇʟᴩ 🛠", callback_data="settings_back_helper"
+            )
+        ],
+         [
+            InlineKeyboardButton(
+                text="ᴄʜᴀɴɴᴇʟ ❣", url=f"https://t.me/rockhushh",
+            )
+            InlineKeyboardButton(
+                text="ꜱᴏᴜʀᴄᴇ 🔗", url=f"https://github.com/Rockhush237",
+            )
+        ],
+        [
+            InlineKeyboardButton(
+                text="ᴀʙᴏᴜᴛ ʀᴏᴄᴋʜᴜꜱʜ 💕", url=f"https://t.me/about_Rockhush",
             )
         ],
      ]
