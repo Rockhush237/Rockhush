818a9baff21b90ef3b1eb6b474c78cf1c4b7ec9c
Rockhush237 <91887560+Rockhush237@users.noreply.github.com>
2023-08-12 14:50:00 +0530

    Update start.py

--- AnonX/utils/inline/start.py
+++ AnonX/utils/inline/start.py
@@ -1,32 +1,7 @@
-from typing import Union
-
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
-
 import config
 
-
-def start_pannel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
-    buttons = [
-        [
-            InlineKeyboardButton(
-                text="ᴀᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴘ",
-                url=f"https://t.me/{BOT_USERNAME}?startgroup=true",
-            )
-        ],
-        [
-            InlineKeyboardButton(
-                text="ʜᴇʟᴩ",
-                callback_data="settings_back_helper",
-            ),
-            InlineKeyboardButton(
-                text="sᴇᴛᴛɪɴɢs", callback_data="settings_helper"
-            ),
-        ],
-     ]
-    return buttons
-
-
-def private_panel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
+def start_panel(_, BOT_USERNAME, OWNER: bool = None):
     buttons = [
         [
             InlineKeyboardButton(
@@ -36,29 +11,56 @@ def private_panel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
         ],
         [
             InlineKeyboardButton(
-                text="ᴏᴜʀ ᴡᴏʀᴅ 💌", url=config.SUPPORT_GROUP
+                text="ʜᴇʟᴩ",
+                callback_data="settings_back_helper",
             ),
             InlineKeyboardButton(
-                text="ᴏᴡɴᴇʀ 💕", user_id=OWNER
+                text="sᴇᴛᴛɪɴɢs",
+                callback_data="settings_helper"
+            ),
+        ],
+    ]
+    return buttons
+
+def private_panel(_, BOT_USERNAME, OWNER: bool = None):
+    buttons = [
+        [
+            InlineKeyboardButton(
+                text="➕ ᴀᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴘ",
+                url=f"https://t.me/{BOT_USERNAME}?startgroup=true",
             )
         ],
         [
             InlineKeyboardButton(
-                text="ʜᴇʟᴩ 🛠", callback_data="settings_back_helper"
-            )
-        ],
-         [
+                text="ᴏᴜʀ ᴡᴏʀᴅ 💌",
+                url=config.SUPPORT_GROUP
+            ),
             InlineKeyboardButton(
-                text="ᴄʜᴀɴɴᴇʟ ❣", url=f"https://t.me/rockhushh",
-            )
-            InlineKeyboardButton(
-                text="ꜱᴏᴜʀᴄᴇ 🔗", url=f"https://github.com/Rockhush237",
+                text="ᴏᴡɴᴇʀ 💕",
+                callback_data="owner_callback"
             )
         ],
         [
             InlineKeyboardButton(
-                text="ᴀʙᴏᴜᴛ ʀᴏᴄᴋʜᴜꜱʜ 💕", url=f"https://t.me/about_Rockhush",
+                text="ʜᴇʟᴩ 🛠",
+                callback_data="settings_back_helper"
             )
         ],
-     ]
+        [
+            InlineKeyboardButton(
+                text="ᴄʜᴀɴɴᴇʟ ❣",
+                url="https://t.me/rockhushh"
+            ),
+            InlineKeyboardButton(
+                text="ꜱᴏᴜʀᴄᴇ 🔗",
+                url="https://github.com/Rockhush237"
+            ),
+        ],
+        [
+            InlineKeyboardButton(
+                text="ᴀʙᴏᴜᴛ ʀᴏᴄᴋʜᴜꜱʜ 💕",
+                url="https://t.me/about_Rockhush"
+            ),
+        ],
+    ]
     return buttons
