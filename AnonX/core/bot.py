0adf26c79f7570176bf5fcde6a7a7842913876e9
Rockhush237 <91887560+Rockhush237@users.noreply.github.com>
2023-08-12 15:44:47 +0530

    Update bot.py

--- AnonX/core/bot.py
+++ AnonX/core/bot.py
@@ -8,11 +8,11 @@ from ..logging import LOGGER
 
 
 
-class AnonXBot(Client):
+class amBot(Client):
     def __init__(self):
         LOGGER(__name__).info(f"Starting Bot...")
         super().__init__(
-            "AnonXMusic",
+            "amMusic",
             api_id=config.API_ID,
             api_hash=config.API_HASH,
             bot_token=config.BOT_TOKEN,
@@ -23,17 +23,8 @@ class AnonXBot(Client):
         get_me = await self.get_me()
         self.username = get_me.username
         self.id = get_me.id
-        if get_me.last_name:
-            self.name = get_me.first_name + " " + get_me.last_name
-        else:
-            self.name = get_me.first_name
-        a = await self.get_chat_member(config.LOG_GROUP_ID, self.id)
-        if a.status != "administrator":
-            LOGGER(__name__).error(
-                "Please promote Bot as Admin in Logger Group"
-            )
-            sys.exit()
-        LOGGER(__name__).info(f"MusicBot Started as {self.name}")
+        self.name = get_me.first_name + " " + (get_me.last_name or "")
+
         try:
             await self.send_message(
                 config.LOG_GROUP_ID, f"**» {config.MUSIC_BOT_NAME} ʙᴏᴛ sᴛᴀʀᴛᴇᴅ :**\n\n✨ ɪᴅ : `{self.id}`\n❄ ɴᴀᴍᴇ : {self.name}\n💫 ᴜsᴇʀɴᴀᴍᴇ : @{self.username}"
@@ -43,3 +34,11 @@ class AnonXBot(Client):
                 "Bot has failed to access the log Group. Make sure that you have added your bot to your log channel and promoted as admin!"
             )
             sys.exit()
+
+        a = await self.get_chat_member(config.LOG_GROUP_ID, self.id)
+        if a.status != "administrator":
+            LOGGER(__name__).error(
+                "Please promote Bot as Admin in Logger Group"
+            )
+            sys.exit()
+        LOGGER(__name__).info(f"MusicBot Started as @{self.username}")
